d2d96f62c9febe8649fb0f2659c584bad6c1e7ae
Moustafa <Moustafamahmoud5600@gmail.com>
2024-10-11 22:20:07 +0200

    @Moustafa

--- Learnova_Angular/Learnova/src/app/Components/instructors/instructors.component.ts
+++ Learnova_Angular/Learnova/src/app/Components/instructors/instructors.component.ts
@@ -1,12 +1,103 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-instructors',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './instructors.component.html',
-  styleUrl: './instructors.component.css'
+  styleUrl: './instructors.component.css',
 })
-export class InstructorsComponent {
+export class InstructorsComponent implements OnInit {
+  experts: Expert[] = [];
 
+  ngOnInit(): void {
+    this.experts = experts;
+  }
 }
+
+export interface Expert {
+  name: string;
+  role: string;
+  image: string;
+}
+
+export const experts: Expert[] = [
+  {
+    name: ' Mohamed El-Desoky',
+    role: 'Coding Instractour',
+    image: 'Images/Inst_img/d.jpeg',
+  },
+  {
+    name: 'Osama Mohamed',
+    role: 'Coding Instractour',
+    image: 'Images/Inst_img/z.jpeg',
+  },
+  {
+    name: 'Mohammed Issa ',
+    role: 'Coding Instractour',
+    image: 'Images/Inst_img/Mohammed_Issa.jpeg',
+  },
+  {
+    name: 'Ahmed Nazmy',
+    role: 'IT&Network Instractour',
+    image: 'Images/Inst_img/Ahmed_N.jpeg',
+  },
+  {
+    name: 'Moustafa Makram',
+    role: 'Graphic Designer Instractour',
+    image: 'Images/Inst_img/Moustafa_M.jpeg',
+  },
+  {
+    name: ' Mohamed El-Desoky',
+    role: 'Coding Instractour',
+    image: 'Images/Inst_img/d.jpeg',
+  },
+  {
+    name: 'Osama Mohamed',
+    role: 'Coding Instractour',
+    image: 'Images/Inst_img/z.jpeg',
+  },
+  {
+    name: 'Mohammed Issa ',
+    role: 'Coding Instractour',
+    image: 'Images/Inst_img/Mohammed_Issa.jpeg',
+  },
+  {
+    name: 'Ahmed Nazmy',
+    role: 'IT&Network Instractour',
+    image: 'Images/Inst_img/Ahmed_N.jpeg',
+  },
+  {
+    name: 'Moustafa Makram',
+    role: 'Graphic Designer Instractour',
+    image: 'Images/Inst_img/Moustafa_M.jpeg',
+  },
+  {
+    name: ' Mohamed El-Desoky',
+    role: 'Coding Instractour',
+    image: 'Images/Inst_img/d.jpeg',
+  },
+  {
+    name: 'Osama Mohamed',
+    role: 'Coding Instractour',
+    image: 'Images/Inst_img/z.jpeg',
+  },
+  {
+    name: 'Mohammed Issa ',
+    role: 'Coding Instractour',
+    image: 'Images/Inst_img/Mohammed_Issa.jpeg',
+  },
+  {
+    name: 'Ahmed Nazmy',
+    role: 'IT&Network Instractour',
+    image: 'Images/Inst_img/Ahmed_N.jpeg',
+  },
+  {
+    name: 'Moustafa Makram',
+    role: 'Graphic Designer Instractour',
+    image: 'Images/Inst_img/Moustafa_M.jpeg',
+  },
+];
