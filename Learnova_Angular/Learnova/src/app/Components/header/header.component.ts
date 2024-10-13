195d7d94a7a95d0909997f2d3e6a6556532d447a
Moustafa <Moustafamahmoud5600@gmail.com>
2024-10-13 21:47:51 +0200

    @Moustafa

--- Learnova_Angular/Learnova/src/app/Components/header/header.component.ts
+++ Learnova_Angular/Learnova/src/app/Components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {   RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ RouterModule],
+  imports: [FormsModule, RouterModule,RouterLink,RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
