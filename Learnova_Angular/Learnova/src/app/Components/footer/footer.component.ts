07537192c819643d0bb0a4eb719dc162df7e96cb
ayaabbas286 <ayaabbas286@gmail.com>
2024-10-25 20:16:23 +0300

    @aya Error Component

--- Learnova_Angular/Learnova/src/app/Components/footer/footer.component.ts
+++ Learnova_Angular/Learnova/src/app/Components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
