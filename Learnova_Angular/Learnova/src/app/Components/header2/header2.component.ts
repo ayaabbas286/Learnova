ad9b69b5c81814b839d2f72c27a3abf61687f5f6
ayaabbas286 <ayaabbas286@gmail.com>
2024-10-27 18:12:29 +0300

    cart&header@aya

--- Learnova_Angular/Learnova/src/app/Components/header2/header2.component.ts
+++ Learnova_Angular/Learnova/src/app/Components/header2/header2.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header2',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './header2.component.html',
   styleUrl: './header2.component.css'
 })
