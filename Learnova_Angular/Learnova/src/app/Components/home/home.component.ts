4127344701ce7b97ab129bc52ac5b8c38878616c
ayaabbas286 <ayaabbas286@gmail.com>
2024-10-31 17:03:16 +0300

    @aya  updates

--- Learnova_Angular/Learnova/src/app/Components/home/home.component.ts
+++ Learnova_Angular/Learnova/src/app/Components/home/home.component.ts
@@ -1,14 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+import { Expert, InstructorsComponent } from '../instructors/instructors.component';
+import { InstructorsServiceService } from '../Instructors-serv/instructors-service.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, RouterLink, RouterModule,RouterLinkActive,RouterOutlet],
+  imports: [FormsModule, RouterLink, RouterModule,RouterLinkActive,RouterOutlet,CommonModule, InstructorsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit  {
+  experts: Expert[] = [];
+
+  constructor(private instructorsService: InstructorsServiceService) {}
+
+  ngOnInit(): void {
+    this.experts = this.instructorsService.getExperts();
+  }
+
+  getExpertGroups(array: Expert[], groupSize: number): Expert[][] {
+    const groups: Expert[][] = [];
+    for (let i = 0; i < array.length; i += groupSize) {
+      groups.push(array.slice(i, i + groupSize));
+    }
+    return groups;
+  }
 
 }
